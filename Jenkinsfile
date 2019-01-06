985de29302dfa12e5d06b6b434663195ac5f9087
Navin Tirumal Kotamraju <navin.kotamraju@yash.com>
2019-01-06 12:50:27 +0530

    yaml1

---
+++ Jenkinsfile
@@ -0,0 +1,14 @@
+@Library('jenkins-library@master')
+
+import 
+com.jenkins.pipeline.Pipeline
+
+
+
+YamlTest
+p = new 
+YamlTest()
+
+
+
+p.start(this,'Node_Ub')
---
+++ src/main/java/com/pack/program/Addition.java
@@ -0,0 +1,16 @@
+package com.pack.program;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10,b=20,c;
+		c= a+b;
+
+		System.out.println("sum of two numbers "+ c);
+		
+		
+	}
+
+}
